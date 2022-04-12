b6d4bfc3a1de76ed1d8a143d427205e1725ffe9c
md ashique <ashique@mobikasa.com>
2022-04-12 23:25:39 +0530

    made changes into tahcartres

--- assets/extra_js.js
+++ assets/extra_js.js
@@ -1,10 +1,13 @@
 let arrrr=[];
 
   $(document).on('click', '#th_cart_btn_c', function () {
-    let th_cart_p_id_ = $(this).attr("th_cart_item_p_id");
+    //let th_cart_p_id_ = $(this).attr("th_cart_item_p_id");
     let th_cart_p_id = $(this).attr("th_cart_item_p_handle");
     $(this).addClass("change_icon_c");
-    $(this).after("<span id='th_cart_added'>item is added to try at cart</span>");
+    // if(parseInt(th_cart_items_id_no) != 6 && !arrrr.includes(th_cart_p_id)){
+    //   $(this).closest('#th_cart_added').remove();
+    // $(this).after("<span id='th_cart_added'>item is added to try at cart</span>");
+    // }
   //  alert("The paragraph was clicked.");
    
 
@@ -17,27 +20,36 @@ let arrrr=[];
             var new_arry=localStorage.getItem("th_cart_items_id");
             const myArr = new_arry.split(",");
             arrrr=myArr;
+            if(parseInt(th_cart_items_id_no) != 4){
             if ( !arrrr.includes(th_cart_p_id)){
+              if(parseInt(th_cart_items_id_no) != 4 && !arrrr.includes(th_cart_p_id)){  
+
             arrrr.push(th_cart_p_id);
             localStorage.setItem("th_cart_items_id",arrrr);
+            $(this).after("<span id='th_cart_added'>item is added to try at cart</span>");
+              }
             }else{
+            $(this).closest('#th_cart_added').remove();
             $(this).after("<span id='th_cart_added'>item is already added to try at cart</span>");
 
             }
-          //document.getElementById("result_item").innerHTML = localStorage.getItem("array_name");
+          }else{
+            $(this).closest('#th_cart_added').remove();
+            $(this).after("<span id='th_cart_added'>your try at home cart is full.</span>");
           }
-
     
+          }
+          
          }
     
+       
         if (localStorage.getItem("th_cart_items_id") === null || parseInt(localStorage.getItem("th_cart_items_id").length) == 0 ) {
     
              arrrr.push(th_cart_p_id);
             localStorage.setItem("th_cart_items_id",arrrr);
             console.log(localStorage.getItem("th_cart_items_id"));
             console.log(localStorage.getItem("th_cart_items_id").length);
-    
-        // document.getElementById("result_item").innerHTML = localStorage.getItem("array_name");
+  
         }
     
 
